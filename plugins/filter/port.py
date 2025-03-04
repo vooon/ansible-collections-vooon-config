c3cc470f4779d558523ef4710ba565f9bac34f04
Vladimir Ermakov <vooon341@gmail.com>
2025-03-04 14:24:30 +0100

    filter: move port and url_replace to fishos.config collection

---
+++ plugins/filter/port.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+from collections.abc import Iterable
+
+from ansible.errors import AnsibleFilterError, AnsibleFilterTypeError
+from ansible.module_utils.six import string_types
+from ansible.utils.display import Display
+from jinja2.exceptions import UndefinedError
+from jinja2.filters import pass_context
+
+display = Display()
+
+
+@pass_context
+def port(context, key):
+    """Find port for service.name"""
+    if not isinstance(key, string_types):
+        raise AnsibleFilterTypeError(f"key should be string, got: {key!r}")
+
+    def getport(varname):
+        m = context.resolve(varname)
+
+        k = key.split(".")
+        if len(k) != 2:
+            raise AnsibleFilterError(
+                "key should be in following format: service_name.port_name"
+            )
+
+        service_name, port_name = k
+        alt_port_name = f"{port_name}_port"
+
+        if service_name not in m:
+            raise UndefinedError(f"{varname}.{service_name}")
+
+        m2 = m[service_name]
+        if port_name in m2:
+            return m2[port_name]
+        elif alt_port_name in m2:
+            return m2[alt_port_name]
+
+        raise UndefinedError(
+            f"{varname}.{service_name}.{port_name} and "
+            f"{varname}.{service_name}.{alt_port_name}"
+        )
+
+    try:
+        return getport("ports_overrides")
+    except UndefinedError:
+        pass
+    except Exception as ex:
+        display.warning(f"ports_overrides exception: {type(ex)} {ex}")
+
+    return getport("ports")
+
+
+@pass_context
+def add_port(context, ip_or_hosts, key):
+    if not isinstance(ip_or_hosts, (string_types, Iterable)):
+        raise AnsibleFilterTypeError(
+            f"ip_or_hosts should be string or list, got: {ip_or_hosts!r}"
+        )
+
+    p = port(context, key)
+    if isinstance(ip_or_hosts, string_types):
+        return f"{ip_or_hosts}:{p}"
+
+    return (f"{host}:{p}" for host in ip_or_hosts)
+
+
+class FilterModule:
+    """Ansible port jinja2 filters"""
+
+    def filters(self):
+        return {
+            "port": port,
+            "add_port": add_port,
+        }
---
+++ plugins/filter/urlreplace.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import typing
+from pathlib import Path
+from urllib.parse import urlparse
+
+from jinja2.filters import pass_context
+
+from .port import port as port_flt
+
+
+@pass_context
+def url_replace(
+    context,
+    url: str,
+    pathadd: typing.Optional[str] = None,
+    path: typing.Optional[str] = None,
+    port: typing.Union[None, str, int] = 0,
+    **kwargs,
+) -> str:
+    """Replace URL parts, like path, port etc."""
+
+    u = urlparse(url)
+
+    if isinstance(port, str):
+        port = port_flt(context, port)
+
+    if (isinstance(port, int) and port > 0) or port is None:
+        netloc = u.netloc.split(":", 1)
+        kwargs["netloc"] = f"{netloc[0]}:{port}" if port else netloc[0]
+
+    if pathadd:
+        kwargs["path"] = str(Path(u.path) / pathadd)
+
+    if path is not None:
+        kwargs["path"] = str(Path(path))
+
+    if kwargs:
+        u = u._replace(**kwargs)
+
+    return u.geturl()
+
+
+class FilterModule:
+    """Ansible jinja2 filters"""
+
+    def filters(self):
+        return {
+            "url_replace": url_replace,
+        }
