6e35e4ee6253bba0ce2e3a8852109aaebcaf275e
Vladimir Ermakov <vooon341@gmail.com>
2025-05-05 20:25:57 +0200

    jsonnet: add params docs

--- plugins/action/jsonnet.py
+++ plugins/action/jsonnet.py
@@ -4,21 +4,25 @@
 
 # https://github.com/luqasn/ansible_jsonnet_template_action
 
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
+
 
 import os
 import shutil
 import stat
 import tempfile
-import _jsonnet
-import yaml
-import json
+from io import StringIO
 
+from ruamel.yaml import YAML
+import _jsonnet
 from ansible import constants as C
 from ansible.config.manager import ensure_type
-from ansible.errors import AnsibleError, AnsibleFileNotFound, AnsibleAction, AnsibleActionFail
-from ansible.module_utils._text import to_bytes, to_text, to_native
+from ansible.errors import (
+    AnsibleAction,
+    AnsibleActionFail,
+    AnsibleError,
+    AnsibleFileNotFound,
+)
+from ansible.module_utils._text import to_bytes, to_native, to_text
 from ansible.module_utils.parsing.convert_bool import boolean
 from ansible.module_utils.six import string_types
 from ansible.plugins.action import ActionBase
@@ -26,16 +30,15 @@ from ansible.template import generate_ansible_template_vars
 
 
 # make yaml prettier
-def str_presenter(dumper, data):
-    if len(data.splitlines()) > 1:  # check for multiline string
-        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
-    return dumper.represent_scalar('tag:yaml.org,2002:str', data)
-
-yaml.add_representer(str, str_presenter)
+# def str_presenter(dumper, data):
+#     if len(data.splitlines()) > 1:  # check for multiline string
+#         return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+#     return dumper.represent_scalar("tag:yaml.org,2002:str", data)
+#
+# yaml.add_representer(str, str_presenter)
 
 
 class ActionModule(ActionBase):
-
     TRANSFERS_FILES = True
 
     def import_callback(self, dirs, rel):
@@ -50,37 +53,40 @@ class ActionModule(ActionBase):
         raise AnsibleError("Unable to find '%s' in expected paths." % to_native(needle))
 
     def run(self, tmp=None, task_vars=None):
-        ''' handler for template operations '''
+        """handler for template operations"""
 
         if task_vars is None:
             task_vars = dict()
 
-        result = super(ActionModule, self).run(tmp, task_vars)
+        result = super().run(tmp, task_vars)
         del tmp  # tmp no longer has any effect
 
         # Options type validation
         # stings
-        for s_type in ('src', 'dest', 'state'):
+        for s_type in ("src", "dest", "state"):
             if s_type in self._task.args:
-                value = ensure_type(self._task.args[s_type], 'string')
+                value = ensure_type(self._task.args[s_type], "string")
                 if value is not None and not isinstance(value, string_types):
-                    raise AnsibleActionFail("%s is expected to be a string, but got %s instead" % (s_type, type(value)))
+                    raise AnsibleActionFail(
+                        "%s is expected to be a string, but got %s instead"
+                        % (s_type, type(value))
+                    )
                 self._task.args[s_type] = value
 
         # booleans
         try:
-            follow = boolean(self._task.args.get('follow', False), strict=False)
+            follow = boolean(self._task.args.get("follow", False), strict=False)
         except TypeError as e:
             raise AnsibleActionFail(to_native(e))
 
         # assign to local vars for ease of use
-        source = self._task.args.get('src', None)
-        dest = self._task.args.get('dest', None)
-        state = self._task.args.get('state', None)
-        format = self._task.args.get('format', 'json')
-        include_dir = self._task.args.get('include_dir', 'templates')
+        source = self._task.args.get("src", None)
+        dest = self._task.args.get("dest", None)
+        state = self._task.args.get("state", None)
+        format = self._task.args.get("format", "json")
+        include_dir = self._task.args.get("include_dir", "templates")
 
-        output_encoding = self._task.args.get('output_encoding', 'utf-8') or 'utf-8'
+        output_encoding = self._task.args.get("output_encoding", "utf-8") or "utf-8"
 
         try:
             # logical validation
@@ -88,46 +94,61 @@ class ActionModule(ActionBase):
                 raise AnsibleActionFail("'state' cannot be specified on a template")
             elif source is None or dest is None:
                 raise AnsibleActionFail("src and dest are required")
-            elif format not in ['json', 'yaml']:
+            elif format not in ["json", "yaml"]:
                 raise AnsibleActionFail("format needs to be either json or yaml")
             else:
                 try:
-                    source = self._find_needle('templates', source)
+                    source = self._find_needle("templates", source)
                 except AnsibleError as e:
                     raise AnsibleActionFail(to_text(e))
 
-            mode = self._task.args.get('mode', None)
-            if mode == 'preserve':
-                mode = '0%03o' % stat.S_IMODE(os.stat(source).st_mode)
+            mode = self._task.args.get("mode", None)
+            if mode == "preserve":
+                mode = "0%03o" % stat.S_IMODE(os.stat(source).st_mode)
 
             # Get vault decrypted tmp file
             try:
                 tmp_source = self._loader.get_real_file(source)
             except AnsibleFileNotFound as e:
-                raise AnsibleActionFail("could not find src=%s, %s" % (source, to_text(e)))
-            b_tmp_source = to_bytes(tmp_source, errors='surrogate_or_strict')
+                raise AnsibleActionFail(
+                    "could not find src=%s, %s" % (source, to_text(e))
+                )
+            b_tmp_source = to_bytes(tmp_source, errors="surrogate_or_strict")
 
             # template the source data locally & get ready to transfer
             try:
-                with open(b_tmp_source, 'rb') as f:
+                with open(b_tmp_source, "rb") as f:
                     try:
-                        template_data = to_text(f.read(), errors='surrogate_or_strict')
+                        template_data = to_text(f.read(), errors="surrogate_or_strict")
                     except UnicodeError:
-                        raise AnsibleActionFail("Template source files must be utf-8 encoded")
+                        raise AnsibleActionFail(
+                            "Template source files must be utf-8 encoded"
+                        )
 
                 # # add ansible 'template' vars
                 temp_vars = task_vars.copy()
                 # temp_vars.update(generate_ansible_template_vars(source, dest))
-                string_vars = { key:str(value) for (key,value) in temp_vars.items()}
+                string_vars = {key: str(value) for (key, value) in temp_vars.items()}
 
                 resultant = _jsonnet.evaluate_snippet(
                     source,
                     template_data,
                     ext_vars=string_vars,
-                    import_callback=lambda dir, rel: self.import_callback([dir, include_dir], rel),
+                    import_callback=lambda dir, rel: self.import_callback(
+                        [dir, include_dir], rel
+                    ),
                 )
-                if format == 'yaml':
-                    resultant = yaml.dump(json.loads(resultant), allow_unicode=True)
+
+                if format == "yaml":
+                    yaml =YAML(typ='safe')
+                    yaml.default_flow_style=False
+                    yaml.indent(mapping=2, sequence=4, offset=2,)
+
+                    original_resultant = yaml.load(StringIO(resultant))
+                    out=StringIO()
+                    yaml.dump(original_resultant, out)
+                    resultant=out.getvalue()
+
             except AnsibleAction:
                 raise
             except Exception as e:
@@ -138,18 +159,26 @@ class ActionModule(ActionBase):
             new_task = self._task.copy()
             # mode is either the mode from task.args or the mode of the source file if the task.args
             # mode == 'preserve'
-            new_task.args['mode'] = mode
+            new_task.args["mode"] = mode
 
             # remove 'template only' options:
-            for remove in ('output_encoding', 'format', 'include_dir'):
+            for remove in ("output_encoding", "format", "include_dir"):
                 new_task.args.pop(remove, None)
 
             local_tempdir = tempfile.mkdtemp(dir=C.DEFAULT_LOCAL_TMP)
 
             try:
                 result_file = os.path.join(local_tempdir, os.path.basename(source))
-                with open(to_bytes(result_file, errors='surrogate_or_strict'), 'wb') as f:
-                    f.write(to_bytes(resultant, encoding=output_encoding, errors='surrogate_or_strict'))
+                with open(
+                    to_bytes(result_file, errors="surrogate_or_strict"), "wb"
+                ) as f:
+                    f.write(
+                        to_bytes(
+                            resultant,
+                            encoding=output_encoding,
+                            errors="surrogate_or_strict",
+                        )
+                    )
 
                 new_task.args.update(
                     dict(
@@ -158,16 +187,18 @@ class ActionModule(ActionBase):
                         follow=follow,
                     ),
                 )
-                copy_action = self._shared_loader_obj.action_loader.get('copy',
-                                                                        task=new_task,
-                                                                        connection=self._connection,
-                                                                        play_context=self._play_context,
-                                                                        loader=self._loader,
-                                                                        templar=self._templar,
-                                                                        shared_loader_obj=self._shared_loader_obj)
+                copy_action = self._shared_loader_obj.action_loader.get(
+                "ansible.legacy.copy",
+                    task=new_task,
+                    connection=self._connection,
+                    play_context=self._play_context,
+                    loader=self._loader,
+                    templar=self._templar,
+                    shared_loader_obj=self._shared_loader_obj,
+                )
                 result.update(copy_action.run(task_vars=task_vars))
             finally:
-                shutil.rmtree(to_bytes(local_tempdir, errors='surrogate_or_strict'))
+                shutil.rmtree(to_bytes(local_tempdir, errors="surrogate_or_strict"))
 
         except AnsibleAction as e:
             result.update(e.result)
---
+++ plugins/modules/jsonnet.py
@@ -0,0 +1,51 @@
+# this is a virtual module that is entirely implemented server side
+
+DOCUMENTATION = """
+---
+module: jsonnet
+short_description: Renders template files using jsonnet format
+description:
+  - The module is an extension of the P(ansible.builtin.copy#module) module and all of attributes that can be
+    set there are available to be set here.
+options:
+  src:
+    description:
+      - Path of a Jsonnet formatted template on the local server. This can be a relative
+        or absolute path.
+    required: true
+    default: null
+  dest:
+    description:
+      - Location to render the template to on the remote machine.
+    required: true
+    default: null
+  format:
+    description:
+      - Template result format
+    default: json
+    choices: [json, yaml]
+  include_dir:
+    description:
+      - Template include dir
+    default: templates
+
+extends_documentation_fragment:
+  - action_common_attributes
+  - action_common_attributes.flow
+  - action_common_attributes.files
+  - backup
+  - files
+  - template_common
+  - validate
+
+author:
+  - Lucas Romero
+"""
+
+EXAMPLES = """
+- name: Run alert template
+  jsonnet:
+    src: node.jsonnet
+    dest: /etc/prometheus/rules/node.yml
+    format: yaml
+"""
